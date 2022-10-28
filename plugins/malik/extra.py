9ff9bb5feb055d04d6f5c7e7851b02543ed65b0f
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2022-10-28 22:56:41 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [FILMY PITARA](https://youtube.com/channel/UCFDpDpnOgcoztiiIkjnJYfw)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [FILMY_PITARA](https://t.me/Rajneesh_Singh_Tomar)**'
+        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [FILMY PITARA](https://youtube.com/channel/UCFDpDpnOgcoztiiIkjnJYfw)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [Vinod Kumar](https://t.me/HACKERVINODSERA)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
