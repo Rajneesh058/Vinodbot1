3e615edf5f6b910b231218fb49c756986bb550dc
Vinodbots <116907014+Vinodbots@users.noreply.github.com>
2022-10-29 10:09:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vinodbots /vinodbots
+  git clone https://github.com/Vinodbots/vinodbots /vinodbots
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /vinodbots
