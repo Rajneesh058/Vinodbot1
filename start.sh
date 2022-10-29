000f8383bcb722dd53ca8abfb0f5a6f50cae4ee6
Vinodbots <116907014+Vinodbots@users.noreply.github.com>
2022-10-29 10:15:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vinodbots/vinodbots /vinodbots
+  git clone https://github.com/Rajneesh058/Vinodbot /vinodbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /vinodbots
+  git clone $UPSTREAM_REPO /vinodbot
 fi
-cd /vinodbots
+cd /vinodbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
